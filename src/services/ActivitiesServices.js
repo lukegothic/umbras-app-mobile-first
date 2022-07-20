84f7e06e7253943a3b95710fdd2c4a635aae17a3
Luke <lukegothic@gmail.com>
2022-07-20 09:51:59 +0200

    endpoint estatico para actividades desde desarrollo

--- src/services/ActivitiesServices.js
+++ src/services/ActivitiesServices.js
@@ -1,7 +1,11 @@
- export const getActivities = async () => {
-    //const request = await fetch("https://dev.alterparadox.es/index.php?rest_route=/alter-api/v1/GetJsonActividades");
-    const request = await fetch("/data/activities.json");
-    const json = request.json();
-    return json;
- }
- 
+export const getActivities = async () => {
+   let activitiesEndpoint = "https://dev.alterparadox.es/index.php?rest_route=/alter-api/v1/GetJsonActividades";
+   if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+      activitiesEndpoint = "/data/activities.json";
+   } else {
+      // pro
+   }
+   const request = await fetch(activitiesEndpoint);
+   const json = request.json();
+   return json;
+}
