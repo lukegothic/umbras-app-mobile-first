b8f0317b3b64fe8de892d0bff26733caafe4a37c
Fermin <fermin.larraya@gmail.com>
2022-08-12 10:17:26 +0200

    mix adescripcioens actividades json y api

--- src/services/ActivitiesServices.js
+++ src/services/ActivitiesServices.js
@@ -1,19 +1,20 @@
-export const getActivities = async () => {
-   // servicio live
-   const activitiesEndpointPROD = "https://dev.alterparadox.es/index.php?rest_route=/alter-api/v1/GetJsonActividades";
-   // archivo estático fallback en caso de estar en dev o si falla el servicio live
-   const activitiesDataDEV = "/data/activities.json";
-   // comprobar si estamos en entorno de DEV
-   const isDevEnv = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
-   // establecer endpoint en funcion de entorno
-   const activitiesEndpoint = isDevEnv ? activitiesDataDEV : activitiesEndpointPROD;
-   // obtener datos
-   let response = await fetch(activitiesEndpoint);
-   // si falla, fallback al archivo estático
-   if (!response.ok) {
-      response = await fetch(activitiesDataDEV);
-   }
-   // obtener como json
-   const json = response.json();
-   return json;
+export const getActivities = async() => {
+    // servicio live
+    const activitiesEndpointPROD = "https://dev.alterparadox.es/index.php?rest_route=/alter-api/v1/GetJsonActividades";
+    // archivo estático fallback en caso de estar en dev o si falla el servicio live
+    const activitiesDataDEV = "/data/activities.json";
+    // comprobar si estamos en entorno de DEV
+    const isDevEnv = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+    // obtener json estático de actividades
+    let response = await fetch(activitiesDataDEV);
+    let activities = await response.json();
+    if (!isDevEnv) { // en pro, obtener datos del endpoint
+        response = await fetch(activitiesEndpointPROD);
+        const activitiesLive = await response.json();
+        // activities es las actividades en vivo y las descripciones 
+        if (response.ok) {
+            activities = activitiesLive.map(activity => ({...activity, LongSummary: activity.LongSummary || activities.find(activity_data => activity.Id === activity_data.Id).LongSummary }));
+        }
+    }
+    return activities;
 }
